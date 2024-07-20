680d679d1bd9533d840899dd14bb9707077a0d8e
josuerx12 <josueaze12@gmail.com>
2024-07-20 18:01:05 -0300

    feat: new infos in home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Button from "@/components/Atoms/Button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h2>Olá mundo</h2>
+      <h2 className="text-center text-xl md:text-3xl font-bold mt-4">
+        Sejá bem vindo ao catalogo JC, encontre seus materiais na página de
+        catalogo de materiais.
+      </h2>
+
+      <p className="text-center text-lg md:text-xl mt-4">
+        Acesse a página de catalogo{" "}
+        <Link className="text-blue-600" href={"/catalogo"}>
+          clicando aqui
+        </Link>
+        . E utilize a pesquisa para encontrar os materiais mais relevantes para
+        seu negócio.
+      </p>
     </main>
   );
 }
