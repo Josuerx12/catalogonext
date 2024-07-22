180be4c79546298a74a4c4b8b3c0fd9e66edad2e
josuerx12 <josueaze12@gmail.com>
2024-07-22 16:58:02 -0300

    feat: new app config added and user profile pic added.

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "catalogo-product-pic.s3.us-east-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "https://userphotoscatalogo.s3.us-east-2.amazonaws.com",
+      },
     ],
   },
 };
