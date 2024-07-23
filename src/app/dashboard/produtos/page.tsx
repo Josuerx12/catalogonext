df1333c082ac699d6d09d40edf631e459ca1b71a
josuerx12 <josueaze12@gmail.com>
2024-07-23 14:10:44 -0300

    feat: new page stucture for dashboard products page.

--- src/app/dashboard/produtos/page.tsx
+++ src/app/dashboard/produtos/page.tsx
@@ -1,7 +1,25 @@
+import ButtonActionsDashboard from "@/components/Molecules/ButtonActionsDashboard";
+import CardListDashboard from "@/components/Organisms/CardListDashboard";
+import { getItems } from "@/services/item-service";
 import React from "react";
 
-const DashboardProducts = () => {
-  return <div>DashboardProducts</div>;
+const DashboardProducts = async ({
+  searchParams,
+}: {
+  searchParams: { name: string; page: string };
+}) => {
+  const { name, page } = searchParams;
+
+  const products = await getItems({ name, page, limit: "20" });
+
+  return (
+    <main className="w-11/12 border mx-auto p-2 rounded-md flex flex-col items-center">
+      <h1 className="text-2xl font-semibold">Dashboard de Produtos</h1>
+      <ButtonActionsDashboard />
+
+      <CardListDashboard items={products} />
+    </main>
+  );
 };
 
 export default DashboardProducts;
