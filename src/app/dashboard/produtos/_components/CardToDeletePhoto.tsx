46fdc2d41b19d6ec3844c9102ced25832ea83f54
josuerx12 <josueaze12@gmail.com>
2024-07-23 16:08:17 -0300

    feat: delete card photo added

---
+++ src/app/dashboard/produtos/_components/CardToDeletePhoto.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Button from "@/components/Atoms/Button";
+import { useMutation, useQueryClient } from "react-query";
+import { deletePhotoOfItem } from "@/services/item-service";
+
+const CardToDeletePhoto = ({
+  photo,
+  productId,
+}: {
+  photo: { _id: string; photo: string };
+  productId: string;
+}) => {
+  const query = useQueryClient();
+  const { isLoading, mutateAsync } = useMutation(
+    ["deletePhoto"],
+    deletePhotoOfItem,
+    {
+      onSuccess: () => query.invalidateQueries("items"),
+    }
+  );
+  return (
+    <div className="flex flex-col gap-3 border p-3 rounded w-fit">
+      <Image
+        className="rounded shadow bg-neutral-400 w-36 h-36 aspect-square object-cover"
+        width={1920}
+        height={1080}
+        src={`https://catalogo-product-pic.s3.us-east-2.amazonaws.com/${photo.photo}`}
+        alt={photo.photo}
+      />
+      <Button
+        type="button"
+        onClick={async () =>
+          await mutateAsync({ photoId: photo._id, productId })
+        }
+        variant="danger"
+        disabled={isLoading}
+      >
+        {isLoading ? "Deletando" : "Deletar"}
+      </Button>
+    </div>
+  );
+};
+
+export default CardToDeletePhoto;
