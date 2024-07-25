f1b0ad5833d2bb00ee55b1010e66a723547fd63b
josuerx12 <josueaze12@gmail.com>
2024-07-25 09:59:51 -0300

    feat: new modal ui design.

--- src/components/Molecules/Modal.tsx
+++ src/components/Molecules/Modal.tsx
@@ -13,15 +13,15 @@ type ModalProps = {
 const Modal = ({ title, children, isOpen, onClose }: ModalProps) => {
   return (
     <section
-      className={`fixed inset-0 m-2 flex flex-col items-center duration-300 justify-start mt-10 md:mt-28  ${
+      className={`fixed inset-0 m-2 flex flex-col items-center duration-300 justify-start mt-10  ${
         isOpen ? " opacity-100 z-50" : "opacity-0 -z-50"
       }`}
     >
       <div
         onClick={onClose}
-        className="fixed inset-0 z-40 w-screen h-screen backdrop-blur "
+        className="fixed inset-0 z-40 w-screen h-screen backdrop-blur"
       />
-      <main className="max-w-screen-lg overflow-auto w-11/12 sm:w-full p-6 bg-neutral-400 text-neutral-50 rounded-md shadow-md z-50">
+      <main className="max-w-screen-lg overflow-auto w-11/12 sm:w-full p-6 bg-neutral-100 text-neutral-800 rounded-md shadow-md shadow-neutral-800 z-50">
         <header className="flex gap-2 justify-between border-b pb-4 items-center">
           <h2 className="text-xl font-semibold">{title}</h2>
           <Button
