6b86be6ce74831be4f8dad0fa173bd173a573ea9
josuerx12 <josueaze12@gmail.com>
2024-07-25 10:00:48 -0300

    refactor: Refactor in card component to optimise quantity of code.

--- src/components/Molecules/ItemCard.tsx
+++ src/components/Molecules/ItemCard.tsx
@@ -1,41 +1,81 @@
+"use client";
+import React, { useState } from "react";
+import Button from "../Atoms/Button";
 import { IItem } from "@/interfaces/items";
+import { Pen, Trash } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-
-const ItemCard = ({ item }: { item: IItem }) => {
+import EditProductModal from "@/app/dashboard/produtos/_components/EditProductModal";
+import ConfirmProductDeletion from "@/app/dashboard/produtos/_components/ConfirmProductDeletion";
+import { useAuth } from "@/contexts/auth-context";
+const ItemCard = ({ product }: { product: IItem }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const { user } = useAuth();
   return (
-    <Link
-      href={`/produto/${item._id}`}
-      className="sm:max-w-[300px] w-full rounded p-2 shadow shadow-neutral-400  hover:bg-neutral-200 hover:shadow-md hover:shadow-neutral-400 duration-200 group"
-    >
-      <h2 className="mb-2 text-center  font-semibold line-clamp-1  duration-200">
-        {item.name}
-      </h2>
-      <Image
-        className="aspect-square  sm:max-w-[300px] w-full object-cover"
-        src={
-          "https://catalogo-product-pic.s3.us-east-2.amazonaws.com/" +
-          item.photos[0]?.photo
-        }
-        alt={item.name}
-        width={1920}
-        height={1080}
+    <>
+      <EditProductModal
+        isOpen={isEditing}
+        item={product}
+        onClose={() => setIsEditing((prev) => !prev)}
       />
-      <p className="pt-2  duration-200">
-        <span className="font-semibold">Categoria: </span>
-        <span className="capitalize">{item.category}</span>
-      </p>
-
-      <p className="  duration-200">
-        <span className="font-semibold">Total disponivel: </span> {item.stock}
-        <span className="text-sm lowercase">{item.unit}</span>
-      </p>
-
-      <p className="opacity-0 py-2 mx-auto text-sm text-center duration-200 font-semibold  group-hover:opacity-100">
-        Clique para mais detalhes
-      </p>
-    </Link>
+      <ConfirmProductDeletion
+        isOpen={isDeleting}
+        item={product}
+        onClose={() => setIsDeleting((prev) => !prev)}
+      />
+      <div
+        className={`border group relative flex flex-wrap sm:flex-nowrap p-4 ${
+          user?.admin && "md:hover:pt-10"
+        }  duration-200 rounded-md w-full   sm:w-96 gap-2`}
+      >
+        <Image
+          src={
+            "https://catalogo-product-pic.s3.us-east-2.amazonaws.com/" +
+            product?.photos[0]?.photo
+          }
+          width={400}
+          height={400}
+          className="md:w-32 w-full md:h-32 aspect-square mx-auto object-cover"
+          alt={"Foto do material" + product.name}
+        />
+        <div className="flex flex-col gap-2">
+          {user?.admin && (
+            <div className="md:absolute md:opacity-0 duration-300 ease-in-out md:group-hover:opacity-100 flex justify-end gap-1 top-2 right-2">
+              <Button
+                onClick={() => setIsDeleting((prev) => !prev)}
+                title={"Deletar produto: " + product.name}
+                variant="danger"
+              >
+                <Trash size={15} />
+              </Button>
+              <Button
+                onClick={() => setIsEditing((prev) => !prev)}
+                title={"Editar produto: " + product.name}
+                variant="primary"
+              >
+                <Pen size={15} />
+              </Button>
+            </div>
+          )}
+          <h2 className="text-xl font-semibold line-clamp-1 h-7 ">
+            {product.name}
+          </h2>
+          <h2 className="text-sm text-gray-600">
+            Estoque: {product.stock} {product.unit}
+          </h2>
+          <p className="text-sm text-gray-600">
+            Descrição: {product.description?.slice(0, 100)}...
+          </p>
+          <Link
+            href={"/produto/" + product._id}
+            className="text-gray-600 hover:text-gray-900 font-semibold w-fit duration-200 mt-auto"
+          >
+            Mais detalhes
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
