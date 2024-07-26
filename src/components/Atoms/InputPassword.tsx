0de462f0a06115ee0ebd16167a71caf8dde0f5b8
josuerx12 <josueaze12@gmail.com>
2024-07-26 08:29:22 -0300

    feat: adding formreft to inputPasswordElement;

--- src/components/Atoms/InputPassword.tsx
+++ src/components/Atoms/InputPassword.tsx
@@ -1,14 +1,15 @@
 "use client";
 import { Eye, EyeOff } from "lucide-react";
-import React, { ComponentProps, useState } from "react";
+import React, { ComponentProps, forwardRef, useState } from "react";
 
 interface InputProps extends ComponentProps<"input"> {}
 
-const InputPassword = (props: InputProps) => {
+const InputPassword = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const [isShowing, setIsShowing] = useState(false);
   return (
     <div className="relative bg-neutral-200 p-2 max-w-sm rounded flex items-center">
       <input
+        ref={ref}
         className="border-none relative bg-neutral-200 placeholder:text-black active:border-2 active:border-neutral-400 max-w-sm rounded outline-none block w-full h-full"
         type={isShowing ? "text" : "password"}
         {...props}
@@ -26,6 +27,8 @@ const InputPassword = (props: InputProps) => {
       )}
     </div>
   );
-};
+});
+
+InputPassword.displayName = "InputPassword";
 
 export default InputPassword;
